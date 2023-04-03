6a1b076a233f13ff3db1768994267b46dd67b2e5
atharv4git <atharv4study@gmail.com>
2023-04-03 22:20:35 +0530

    Landing Page Modified

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>E-Learning</div>
+      <div id='head1'>Elegent Coder</div>
+      <div id='sub1'>
+        <p id='p1'>Why us?</p>
+        <p id='p2'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum nunc aliquet bibendum enim facilisis gravida neque. Id diam vel quam elementum pulvinar etiam non quam. Luctus accumsan tortor posuere ac ut consequat semper. Habitant morbi tristique senectus et netus et. Vitae elementum curabitur vitae nunc. Ornare arcu dui vivamus arcu felis bibendum. Ut tortor pretium viverra suspendisse potenti nullam ac. Sed odio morbi quis commodo odio aenean. Suspendisse interdum consectetur libero id faucibus nisl tincidunt eget nullam. At imperdiet dui accumsan sit amet nulla. In dictum non consectetur a erat. Potenti nullam ac tortor vitae purus faucibus. Nibh venenatis cras sed felis eget velit. Justo laoreet sit amet cursus. Adipiscing tristique risus nec feugiat in. Ultricies mi quis hendrerit dolor magna eget est lorem. Non diam phasellus vestibulum lorem sed risus.
+
+Sem et tortor consequat id porta nibh venenatis cras sed. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. Enim sit amet venenatis urna cursus eget. Aliquam eleifend mi in nulla posuere. Lectus arcu bibendum at varius vel pharetra. Natoque penatibus et magnis dis. Vitae ultricies leo integer malesuada. Euismod quis viverra nibh cras pulvinar mattis nunc sed. Maecenas accumsan lacus vel facilisis volutpat est. Auctor urna nunc id cursus metus aliquam. Libero volutpat sed cras ornare arcu dui vivamus arcu felis. Et sollicitudin ac orci phasellus egestas tellus rutrum tellus. Elementum eu facilisis sed odio morbi quis. Libero nunc consequat interdum varius sit. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Suscipit tellus mauris a diam maecenas sed enim. Interdum velit euismod in pellentesque massa placerat duis ultricies.
+
+Consectetur libero id faucibus nisl tincidunt eget nullam. At erat pellentesque adipiscing commodo. Scelerisque viverra mauris in aliquam sem. Est velit egestas dui id ornare arcu odio ut sem. Nibh sit amet commodo nulla facilisi nullam vehicula ipsum a. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Duis ultricies lacus sed turpis tincidunt id. Magnis dis parturient montes nascetur ridiculus mus mauris. Fringilla ut morbi tincidunt augue. Quisque non tellus orci ac auctor augue.
+        </p>
+        <img id='img1' src='https://media.tenor.com/5qwhjWYm9kIAAAAM/give-me-my-website-bazarack.gif'></img>
+
+      </div>
     </div>
   );
 }
