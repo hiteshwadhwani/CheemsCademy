a3adb2c6bcd366bc39905ad78450865446a2ee23
hiteshwadhwani <developerhitesh1403@gmail.com>
2023-04-15 23:53:39 +0530

    catalog pafe updated

--- src/components/catalog/Catalog.js
+++ src/components/catalog/Catalog.js
@@ -1,22 +1,127 @@
-import { Box, Container, Grid, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Grid,
+  Heading,
+  List,
+  ListItem,
+} from "@chakra-ui/react";
 import PopularSubjects from "./PopularSubjects";
 import TopCarrerPaths from "./TopCarrerPaths";
 import PopularCourses from "./PopularCourses";
 import Boxes from "../welcome/Boxes";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  doc,
+  getDoc,
+  collection,
+  getDocs,
+  collectionGroup,
+  where,
+  query,
+} from "firebase/firestore";
+import { db } from "../../firebase";
 
 const Catalog = (props) => {
+  const [showCourses, setShowCourses] = useState("all");
+  const [courses, setCourses] = useState()
+  const [loading, setLoading] = useState(true)
+  console.log(showCourses);
+
+  const getCourses = async (type) => {
+    let collection_ref = null;
+    if(type === 'all'){
+      collection_ref = collection(db, "courses");
+    }
+    else{
+      collection_ref = query(collection(db, "courses"), where("type", "==", type));
+    }
+    
+    try {
+      const courses = [];
+
+      const ret = await getDocs(collection_ref);
+
+      ret.forEach(async (response) => {
+        let course = {};
+        course["course"] = response.data();
+        const module_arr = [];
+        const modules = await getDocs(
+          collection(db, "courses", response.id, "modules")
+        );
+        modules.forEach((module) => {
+          module_arr.push(module.data());
+        });
+        course["module"] = module_arr;
+        courses.push(course);
+      });
+      setCourses(courses)
+    } catch (err) {
+      console.log("something went wrong in getting courses", err);
+    }
+    finally{
+      setLoading(false)
+    }
+  };
+
+  useState(() => {
+    getCourses(showCourses)
+  }, [showCourses]);
+
+  console.log(courses)
+
   return (
     <>
-      <Box padding='3rem' marginX='15rem'>
+      <Box padding="3rem" marginX="15rem">
         <Grid templateColumns="1fr 3fr">
-          <Box>LEFT</Box>
           <Box>
-            <Heading as="h2" size="xl">
-              Explore the catalog
+            <Heading marginTop="10px" as="h4" size="sm" marginBottom="25px">
+              Top Languages and Subjects
             </Heading>
-            <PopularSubjects />
-            <TopCarrerPaths />
-            <PopularCourses />
+            <List>
+              <ListItem
+                marginBottom="5px"
+                _hover={{ cursor: "pointer" }}
+                onClick={() => setShowCourses("Python")}
+              >
+                Python
+              </ListItem>
+              <ListItem
+                marginBottom="5px"
+                _hover={{ cursor: "pointer" }}
+                onClick={() => setShowCourses("Java")}
+              >
+                Java
+              </ListItem>
+              <ListItem
+                marginBottom="5px"
+                _hover={{ cursor: "pointer" }}
+                onClick={() => setShowCourses("Javascript")}
+              >
+                Javascript
+              </ListItem>
+              <ListItem
+                marginBottom="5px"
+                _hover={{ cursor: "pointer" }}
+                onClick={() => setShowCourses("C++")}
+              >
+                C++
+              </ListItem>
+            </List>
+          </Box>
+          <Box>
+            <Heading as="h2" size="xl" marginBottom="25px">
+              {showCourses === "all"
+                ? "Explore the catalog"
+                : `Explore ${showCourses}`}
+            </Heading>
+            <Grid templateColumns="repeat(3, 1fr)" gap={5}>
+              <Link to="/course/:id">
+                <Boxes />
+              </Link>
+              <Boxes />
+            </Grid>
           </Box>
         </Grid>
       </Box>
--- src/components/welcome/Boxes.js
+++ src/components/welcome/Boxes.js
@@ -1,26 +1,70 @@
-import { useMediaQuery, Divider, Box, Card, CardHeader, CardBody, CardFooter, Heading,Text, Flex } from "@chakra-ui/react"
+import {
+  useMediaQuery,
+  Divider,
+  Box,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Heading,
+  Text,
+  Flex,
+} from "@chakra-ui/react";
+import { transform } from "framer-motion";
+import styled from "styled-components";
 
 const Boxes = (props) => {
-    
+  return (
+    // <Card height='20rem' borderRadius='0' _hover={{transform: 'translate3d(2px, -2px, 2px)'}} transition='transform 0.2s ease'>
+    <CustomCard>
+      <video autoPlay={true} loop={true} playsInline={true} muted>
+        <source src="/videos/Doge dancing.mp4" type="video/mp4" />
+      </video>
+      <CardHeader backgroundColor="rgb(234,253,198)">
+        <Heading size="xs">Client Report</Heading>
+      </CardHeader>
+      <CardBody>
+        <Heading as="h5" size="md">
+          Learn JavaScript: Fundamentals
+        </Heading>
+        <Text pt="2" fontSize="sm">
+          Learn how to use JavaScript to power dynamic behaviors on websites.
+        </Text>
+      </CardBody>
+      <Divider />
+      <CardFooter
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Box>Beginner Friendly</Box>
+        <Box>3 lessons</Box>
+      </CardFooter>
+    </CustomCard>
+    // </Card>
+  );
+};
 
-    return (
-        <Card height='20rem' borderRadius='0'>
-            <CardHeader backgroundColor='rgb(234,253,198)'>
-            <Heading size='xs'>Client Report</Heading>
-            </CardHeader>
-            <CardBody>
-                <Heading as='h5' size='md'>Learn JavaScript: Fundamentals</Heading>
-                <Text pt='2' fontSize='sm'>Learn how to use JavaScript to power dynamic behaviors on websites.</Text>
-            </CardBody>
-            <Divider />
-            <CardFooter display='flex' alignItems='center' justifyContent='space-between'>
-                
-                    <Box>Beginner Friendly</Box>
-                    <Box>3 lessons</Box>
-                
-            </CardFooter>
-        </Card>
-    )
-}
+const CustomCard = styled(Card)`
+  height: 20rem;
+  border-radius: 0;
+  transition: transform 0.2s ease;
+  position: relative;
+  video {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    inset: 0px;
+    z-index: 0;
+    opacity: 0;
+    object-fit: cover;
+  }
+  &:hover {
+    transform: translate3d(2px, -2px, 2px);
+    video {
+      opacity: 0.3;
+    }
+  }
+`;
 
-export default Boxes
+export default Boxes;
